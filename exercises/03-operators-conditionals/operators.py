9272ce887679edf5c3d57897edc4090fa83c9a80
KateLouiseJohnson <katelouisejohnson@gmail.com>
2025-09-21 18:43:21 +0200

    Update operators.py

--- exercises/03-operators-conditionals/operators.py
+++ exercises/03-operators-conditionals/operators.py
@@ -4,7 +4,7 @@
 # - is_it_a_public_holiday
 # Assign True or False values to your variables
 # Create print statements comparing your two variables using the "and" operator
-# Play around with changing the values of your varibales to see what happens to your output
+# Play around with changing the values of your variables to see what happens to your output
 # Try using the "or" operator instead, how does this change your results?
 
 # Example of expected output:
