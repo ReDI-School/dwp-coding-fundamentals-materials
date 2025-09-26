cf1a155dc9ea0ea3058ec078c41ea0784648a12e
Gayathri Vijayan <66356931+grvijayan@users.noreply.github.com>
2025-09-26 20:03:04 +0200

    add exercise for lists

--- exercises/04-loops-lists/lists.py
+++ exercises/04-loops-lists/lists.py
@@ -1,1 +1,14 @@
 # Coding exercises using Lists
+"""
+You’ve made a weekend to-do list in Python, but it needs a little organizing. Use list operations to solve this exercise.
+
+1. "watch Netflix" isn’t very productive. Replace it with "read a book".
+2. You already called your mom. Remove "call mom" from the list.
+3. Add "meal prep" and "take a walk" to the end of the list.
+4. Move "laundry" to the end of the list (Hint: remove it first, then append it.).
+5. Insert "meditate" at the beginning of the list.
+6. Create a new list called top_3 that contains the first three tasks from the updated todo list.
+"""
+
+# My weekend to-do list
+todo = ["laundry", "grocery shopping", "call mom", "watch Netflix", "clean kitchen", "nap"]
