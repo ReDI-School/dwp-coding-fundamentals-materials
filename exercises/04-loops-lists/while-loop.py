28a389de3667cefa4c029406f9c117fd87a33631
Gayathri Vijayan <66356931+grvijayan@users.noreply.github.com>
2025-10-04 10:47:39 +0200

    add an exercise for while loop
    Added a simple playlist player program using a while loop that allows users to repeat, skip, or stop songs.

--- exercises/04-loops-lists/while-loop.py
+++ exercises/04-loops-lists/while-loop.py
@@ -1,1 +1,26 @@
 # Coding exercises using While loop
+"""
+You need to create a simple playlist player program in Python that plays songs from a list one by one. 
+The program should:
+1. Start with the first song in the playlist.
+2. After playing a song, ask the user for input: repeat, next, stop
+3. If the user types repeat, play the same song again.
+4. If the user types next, move on to the next song.
+5. If the user types stop, end the program immediately.
+6. If the user types anything else, print an error message asking for a valid command.
+7. When the playlist reaches the last song and the user chooses next, the program should end with a friendly message.
+
+Use a while loop to control the flow of the playlist.
+"""
+
+# Playlist
+playlist = [
+    "Bohemian Rhapsody - Queen",
+    "Billie Jean - Michael Jackson",
+    "Shape of You - Ed Sheeran",
+    "Blinding Lights - The Weeknd",
+    "Hotel California - Eagles",
+    "Rolling in the Deep - Adele",
+    "Sweet Child O' Mine - Guns N' Roses",
+    "Levitating - Dua Lipa"
+]
