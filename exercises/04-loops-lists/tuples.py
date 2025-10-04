e12456c423c2977f3ecbc87e2550e039c35a2ec5
Gayathri Vijayan <66356931+grvijayan@users.noreply.github.com>
2025-10-04 10:35:56 +0200

    add an exercise for tuples

--- exercises/04-loops-lists/tuples.py
+++ exercises/04-loops-lists/tuples.py
@@ -1,1 +1,20 @@
 # Coding exercises using Tuples
+"""
+You're organizing books in a library.
+
+The books: 
+1. "1984", George Orwell, 1949
+2. "Pride and Prejudice", Jane Austen, 1813
+3. "To Kill a Mockingbird", Harper Lee, 1960
+4. "The Murder of Roger Ackroyd", Agatha Christie, 1926
+5. "11/22/63", Stephen King, 2011
+
+Tasks
+1. Create 3 book tuples with (title, author, year).
+2. Print all the books in the format: 1984, by George Orwell (1949)
+
+# Hint:
+# this is a tuple containing the name of the book, the author, and the publication year
+book1 = ("1984", "George Orwell", 1949) 
+"""
+
